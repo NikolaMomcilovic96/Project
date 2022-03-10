3e1710f9caa9c8835ba58e1eaf01c20479bd9539
NikolaMomcilovic96 <nikolamomca96@gmail.com>
2022-03-10 15:35:46 +0100

    Removed ViewHolder

--- app/src/main/java/com/uncode/ui/main/ResultItemViewHolder.kt
+++
@@ -1,6 +0,0 @@
-package com.uncode.ui.main
-
-import androidx.recyclerview.widget.RecyclerView
-import com.uncode.databinding.RvItemListBinding
-
-class ResultItemViewHolder(private val binding: RvItemListBinding) : RecyclerView.ViewHolder(binding.root)
